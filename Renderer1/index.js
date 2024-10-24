611277a27d7bb627c9d708fa905e4d885c22c83a
SEB03penguin <SEB03penguin@gmail.com>
2024-10-24 11:34:44 +0100

    a

--- Renderer1/index.js
+++ Renderer1/index.js
@@ -25,7 +25,14 @@ function matrixMultiply(value,matrix) {
 function start() {
     Canvas.start();
     camera = new createCamera();
-    cube1 = new createCube();
+
+    let v, f = loadObjFromUrl('SEB03penguin.github.io/Renderer1/suzanne.obj')
+
+    cube1 = new createObject(0, [255,0,0], vertices=v, faces=f);
+    cube2 = new createObject(3, [0,255,0]);
+    cube3 = new createObject(-3,[0,0,255]);
+
+
 
     setInterval(updateCanvas, 20);
 }
@@ -40,9 +47,9 @@ var Canvas = {
     }
 }
 
-function drawLine(p1, p2) {
+function drawLine(p1, p2, colour) {
     ctx = Canvas.context;
-    ctx.strokeStyle = "white";
+    ctx.strokeStyle = colour;
     ctx.lineWidth = 2;
 
     ctx.beginPath();
@@ -51,10 +58,27 @@ function drawLine(p1, p2) {
     ctx.stroke();
 }
 
-function drawTriangle(p1, p2, p3) {
-    drawLine(p1, p2);
-    drawLine(p2, p3);
-    drawLine(p3, p1);
+function drawTriangle(p1, p2, p3, colour) {
+    let wireframe = false
+    if (wireframe) {
+        drawLine(p1, p2, rgb(colour));
+        drawLine(p2, p3, rgb(colour));
+        drawLine(p3, p1, rgb(colour));
+    } else {
+        drawLine(p1, p2, "white");
+        drawLine(p2, p3, "white");
+        drawLine(p3, p1, "white");
+
+        ctx = Canvas.context;
+        ctx.beginPath();
+        ctx.moveTo(p1.x, p1.y);
+        ctx.lineTo(p2.x, p2.y);
+        ctx.lineTo(p3.x, p3.y);
+        ctx.closePath();
+        console.log(colour);
+        ctx.fillStyle = `rgb(${colour})`;
+        ctx.fill();
+    }
 }
 
 function createCamera() {
@@ -158,42 +182,96 @@ function createCamera() {
     };
 }
 
-function createCube() {
+cubeVert=[
+    {v1:0,v2:1,v3:2},
+    {v1:0,v2:2,v3:3},
 
-    this.vertexList=[
-        {x:-1,y:-1,z:-1},
-        {x:-1,y:1,z:-1},
-        {x:1,y:1,z:-1},
-        {x:1,y:-1,z:-1},
-        {x:1,y:-1,z:1},
-        {x:1,y:1,z:1},
-        {x:-1,y:1,z:1},
-        {x:-1,y:-1,z:1},
-    ]
+    {v1:3,v2:2,v3:5},
+    {v1:3,v2:4,v3:4},
 
-    this.triangleList=[
-        {v1:0,v2:1,v3:2},
-        {v1:0,v2:2,v3:3},
+    {v1:4,v2:5,v3:6},
+    {v1:4,v2:6,v3:7},
 
-        {v1:3,v2:2,v3:5},
-        {v1:3,v2:4,v3:4},
+    {v1:7,v2:6,v3:1},
+    {v1:7,v2:1,v3:0},
 
-        {v1:4,v2:5,v3:6},
-        {v1:4,v2:6,v3:7},
+    {v1:1,v2:6,v3:5},
+    {v1:1,v2:5,v3:2},
+    
+    {v1:0,v2:7,v3:4},
+    {v1:0,v2:4,v3:3},
+]
+cubeFace=[
+    {v1:0,v2:1,v3:2},
+    {v1:0,v2:2,v3:3},
 
-        {v1:7,v2:6,v3:1},
-        {v1:7,v2:1,v3:0},
+    {v1:3,v2:2,v3:5},
+    {v1:3,v2:4,v3:4},
 
-        {v1:1,v2:6,v3:5},
-        {v1:1,v2:5,v3:2},
+    {v1:4,v2:5,v3:6},
+    {v1:4,v2:6,v3:7},
+
+    {v1:7,v2:6,v3:1},
+    {v1:7,v2:1,v3:0},
+
+    {v1:1,v2:6,v3:5},
+    {v1:1,v2:5,v3:2},
+    
+    {v1:0,v2:7,v3:4},
+    {v1:0,v2:4,v3:3},
+]
+
+function loadObjFromUrl(url) {
+    try {
+        file = fetch(url).then(response => response.text())
+        console.log("couldnt load obj from url")
+        return
+    } catch {
+
+    }
+
+    const lines = file.split('\n');
+    const vertices = [];
+    const faces = [];
+
+    lines.forEach(line => {
+        const parts = line.trim().split(' ');
         
-        {v1:0,v2:7,v3:4},
-        {v1:0,v2:4,v3:3},
-    ]
+        if (parts[0] === 'v') {
+            // It's a vertex line
+            vertices.push({
+                x: parseFloat(parts[1]),
+                y: parseFloat(parts[2]),
+                z: parseFloat(parts[3])
+            });
+        } else if (parts[0] === 'f') {
+            // It's a face line
+            faces.push({
+                v1: parseInt(parts[1]),
+                v2: parseInt(parts[2]),
+                v3: parseInt(parts[3])
+            });
+        }
+    });
+    
+    console.log('Vertices:', vertices);
+    console.log('Faces:', faces);
+
+    return vertices, faces
+
+}
+
+
+function createObject(offset, colour, vertices=cubeVert, faces=cubeFace) {
+    this.colour = colour
+
+    this.vertexList=vertices
+
+    this.faceList=faces
 
     this.draw = function() {
 
-        fTheta +=0.05;
+        fTheta +=0.005;
 
         let matRotZ = [
             [0, 0, 0, 0],
@@ -217,7 +295,7 @@ function createCube() {
         matRotX[2][1] = -Math.sin(fTheta * 0.5);
         matRotX[2][2] = Math.cos(fTheta * 0.5);
 
-        for (const triangle of this.triangleList) {
+        for (const triangle of this.faceList) {
 
             let v1RotatedZ = matrixMultiply(this.vertexList[triangle.v1], matRotZ);
             let v2RotatedZ = matrixMultiply(this.vertexList[triangle.v2], matRotZ);
@@ -231,26 +309,67 @@ function createCube() {
             let v1Translated = v1RotatedZX;
             let v2Translated = v2RotatedZX;
             let v3Translated = v3RotatedZX;
-            v1Translated.z += 3
-            v2Translated.z += 3
-            v3Translated.z += 3
+            v1Translated.z += 6;
+            v2Translated.z += 6;
+            v3Translated.z += 6;
+
+            let line1 = {};
+            line1.x = v2Translated.x - v1Translated.x;
+            line1.y = v2Translated.y - v1Translated.y;
+            line1.z = v2Translated.z - v1Translated.z;
+            let line2 = {};
+            line2.x = v3Translated.x - v1Translated.x;
+            line2.y = v3Translated.y - v1Translated.y;
+            line2.z = v3Translated.z - v1Translated.z;
+
+            //dot product
+            let normal = {};
+            normal.x = line1.y * line2.z - line1.z * line2.y;
+            normal.y = line1.z * line2.x - line1.x * line2.z;
+            normal.z = line1.x * line2.y - line1.y * line2.x;
+
+            //normalise normal
+            let normalLen = Math.sqrt(normal.x*normal.x + normal.y*normal.y + normal.z*normal.z);
+            normal.x /= normalLen; normal.y /= normalLen; normal.z /= normalLen;
+
+            // if (normal.z < 0) {
+            if (normal.x * (v1Translated.x - camera.pos.x) +
+                normal.y * (v1Translated.y - camera.pos.y) +
+                normal.z * (v1Translated.z - camera.pos.z) < 0) {
+
+                //lighting
+                let light_direction = {x:0, y:0, z:-1}
+                let Len = Math.sqrt(light_direction.x*light_direction.x + light_direction.y*light_direction.y + light_direction.z*light_direction.z);
+                light_direction.x /= Len;
+                light_direction.y /= Len;
+                light_direction.z /= Len;
+
+                let dot_product = normal.x * light_direction.x + normal.y * light_direction.y + normal.z * light_direction.z;
+                dot_product = Math.max(0, Math.min(1, dot_product));
+
+                let face_colour = [
+                    this.colour[0] * dot_product,
+                    this.colour[1] * dot_product,
+                    this.colour[2] * dot_product
+                ];
+
+                //project from 3d to 2d
+                let p1 = matrixMultiply(v1Translated, camera.matProj);
+                let p2 = matrixMultiply(v2Translated, camera.matProj);
+                let p3 = matrixMultiply(v3Translated, camera.matProj);
+                
+                //scale into view
+                p1.x += 1; p1.y += 1;
+                p2.x += 1; p2.y += 1;
+                p3.x += 1; p3.y += 1;
 
 
-            let p1 = matrixMultiply(v1Translated, camera.matProj)
-            let p2 = matrixMultiply(v2Translated, camera.matProj)
-            let p3 = matrixMultiply(v3Translated, camera.matProj)
-            
-            //scale into view
-            p1.x += 1; p1.y += 1;
-            p2.x += 1; p2.y += 1;
-            p3.x += 1; p3.y += 1;
+                p1.x *= 0.5*canvasWidth; p1.y *= 0.5*canvasHeight;
+                p2.x *= 0.5*canvasWidth; p2.y *= 0.5*canvasHeight;
+                p3.x *= 0.5*canvasWidth; p3.y *= 0.5*canvasHeight;
 
-
-            p1.x *= 0.5*canvasWidth; p1.y *= 0.5*canvasHeight;
-            p2.x *= 0.5*canvasWidth; p2.y *= 0.5*canvasHeight;
-            p3.x *= 0.5*canvasWidth; p3.y *= 0.5*canvasHeight;
-
-            drawTriangle(p1, p2, p3)
+                drawTriangle(p1, p2, p3, face_colour)
+            }
         }
         
 
@@ -276,9 +395,10 @@ function updateCanvas() {
     ctx = Canvas.context;
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-
-    cube1.draw();
     
+    cube1.draw();
+    cube2.draw();
+    cube3.draw();
 }
 
 
