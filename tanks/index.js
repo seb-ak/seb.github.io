90fef3dbfd285aab32096a10b9b611c4a83b7d26
seb <108451832+seb-ak@users.noreply.github.com>
2026-01-03 14:35:51 +0000

    actually fixed menu

--- tanks/index.js
+++ tanks/index.js
@@ -464,9 +464,8 @@ function joinServer(url, name, colour) {
     main.isPlaying = true;  
     main.wsStart(url);      localStorage.setItem('url', url);
 
-    const menu = document.getElementById('menuContainer');
-    if (menu) { menu.style.visibility = "hidden"; }
-
+    document.getElementById('menuContainer').style.visibility = "hidden";
+    document.getElementById('menu').style.visibility = "hidden";
  
 
     const thisurl = new URL(location.href);
